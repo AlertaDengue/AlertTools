671932286ca20efb684188370e42720b7f7212c1
claudia-codeco <claudia.codeco@gmail.com>
2023-09-18 16:30:38 -0300

    function to get satellite data created

--- R/get_timeseries.R
+++ R/get_timeseries.R
@@ -137,6 +137,66 @@ bestWU <- function(series,var){
             }
 }
 
+# getClima --------------------------------------------------------
+#'@description Create weekly climate time series from satellite data in server 
+#'taking the mean of the daily values.
+#'@title Get Climate Data
+#'@export
+#'@param cities list of geocodes.
+#'@param vars vector with meteorological variables. Minimum set of vars 
+#'"temp_min", "temp_max". Default: all of them
+#'@param iniSE first epiweek. Default is 012015 
+#'@param lastSE last epiweek. Optional. Default is the last available
+#'@param finalday alternative to lastSE. Default is the last day available. Format = Y-m-d. 
+#'@param datasource a valid database connection 
+#'@return data.frame with the weekly data (cidade data temp_min tmed tmax umin umed umax pressaomin pressaomed pressaomax)
+#'@examples
+#'NOT USE: con <- dbConnect(RSQLite::SQLite(), "../../AlertaDengueAnalise/mydengue.sqlite")
+#'res = getClima(cities = c(3304557), vars=c("temp_min","temp_max") , iniSE = 201801)
+#'res = getClima(cities = c(3304557, 3200300), iniSE = 202003)
+#'tail(res)
+
+getClima <- function(cities, vars = c("temp_min","temp_max","temp_med","umid_min",
+                                      "umid_med","umid_max", "precip_tot","precip_max"),
+                     finalday = Sys.Date(), iniSE = 201501, lastSE, datasource=con) {
+      
+      # validate climate variables
+      if(any(!(vars %in% vars))) stop("climate variable(s) unknown or mispecified")
+      
+      assert_that(all(c("temp_min","temp_max") %in% vars), msg="getClima: vars should 
+                                                           include temp_min, temp_max. Check it!")
+      # dates
+      #if(missing(lastSE)) lastSE <- epiweek(finalday, format = "Y-m-d")
+      iniday <- SE2date(iniSE)$ini
+      
+      if(class(datasource) == "PostgreSQLConnection"){
+            sqlcity = paste("'", str_c(cities, collapse = "','"),"'", sep="")
+            varscomplete = c("geocodigo", "date", vars)
+            sqlvars = paste("", str_c(varscomplete, collapse = ","), sep="")
+            
+            comando <- paste("SELECT ", sqlvars ," from \"weather\".\"copernicus_brasil\" 
+                        WHERE geocodigo IN  (", sqlcity, ") AND 
+                         date <= '",finalday,"' AND date >= '", iniday, "'",sep="")
+            
+            d <- dbGetQuery(datasource,comando) 
+      }
+      
+      # agregando vars climaticas por semana
+      vars <- c(vars, "ampT") # calculating daily thermal amplitude
+      d1 <- d %>% 
+            mutate(SE = data2SE(date, format = "%Y-%m-%d")) %>% # creating column SE
+            mutate(ampT = temp_max - temp_min) %>% 
+            group_by(geocodigo,SE)  %>%
+            summarise_at(vars(vars),list(mean),na.rm=TRUE) 
+      
+      # check output-----------------------------------------
+      assert_that(all(c("geocodigo","SE") %in% names (d1)), msg = "debug getClima required")
+      
+      d1
+      
+}
+
+
 # getTweet --------------------------------------------------------------
 #'@description Create weekly time series from tweeter data from server. The 
 #'source of this data is the Observatorio da Dengue (UFMG).
